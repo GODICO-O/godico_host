79bb13c550a7e8c8fe2fae92f4ca01c02aba7862
GODICO-O <rizkiramadhan2300@gmail.com>
2026-06-23 01:56:14 +0700

    Auto-build: Perbaikan sistem dan penyelarasan biner

--- rust_core/src/lib.rs
+++ rust_core/src/lib.rs
@@ -4,12 +4,10 @@ use std::io::{Read, Write};
 #[no_mangle]
 #[allow(non_snake_case)]
 pub extern "C" fn Java_com_godico_devhub_MainActivity_startIpcServer(
-    _env: *mut std::ffi::c_void,
-    _jclass: *mut std::ffi::c_void,
+    _env: *mut std::ffi::c_void,                    _jclass: *mut std::ffi::c_void,
 ) {
     // Jalankan server di dalam thread terpisah agar runtime Java Android tetap bernapas lega
-    std::thread::spawn(|| {
-        match TcpListener::bind("0.0.0.0:8080") {
+    std::thread::spawn(|| {                             match TcpListener::bind("0.0.0.0:8080") {
             Ok(listener) => {
                 for stream in listener.incoming() {
                     match stream {
@@ -17,9 +15,15 @@ pub extern "C" fn Java_com_godico_devhub_MainActivity_startIpcServer(
                             let mut buffer = [0; 128];
                             if let Ok(bytes_read) = s.read(&mut buffer) {
                                 let pesan = String::from_utf8_lossy(&buffer[..bytes_read]);
+                                
+                                // JALUR 1: TES PING UTAMA (SETELAN PABRIK)
                                 if pesan.contains("STUDIO:VIDEO") {
-                                    // Sinyal balik sementara untuk membuktikan JNI dan Port 8080 Hidup Sehat!
                                     s.write_all(b"STATUS:JNI_ALIVE").unwrap();
+                                }
+                                
+                                // JALUR 2: PERINTAH BARU UNTUK LAMPU HIJAU
+                                else if pesan.contains("LAMP:HIJAU") {
+                                    s.write_all(b"STATUS:LAMP_GREEN_ACTIVE_OK").unwrap();
                                 }
                             }
                         }
