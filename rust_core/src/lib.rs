465f7bbde864ef9b2e8cc3388576ab464094c76d
GODICO-O <rizkiramadhan2300@gmail.com>
2026-07-01 18:03:01 +0700

    Auto-deploy: Sinkronisasi kode dan trigger CI/CD

--- rust_core/src/lib.rs
+++ rust_core/src/lib.rs
@@ -5,15 +5,15 @@ fn android_main(app: AndroidApp) {
     android_logger::init_once(android_logger::Config::default().with_tag("GODICO_NATIVE"));
     log::info!("GODICO NATIVE: Engine Starting...");
 
-    // Event Loop yang efisien dan smooth
-    app.poll_events(Some(std::time::Duration::from_millis(10)), |event| {
+    app.poll_events(Some(std::time::Duration::from_millis(16)), |event| {
         match event {
             PollEvent::Main(event) => match event {
-                MainEvent::InitWindow => {
-                    log::info!("GODICO NATIVE: Window Ready - Render Start");
+                // Semua event di v0.5.2 harus menggunakan sintaks { .. }
+                MainEvent::InitWindow { .. } => {
+                    log::info!("GODICO NATIVE: Window Ready");
                 }
                 MainEvent::Resume { .. } => {
-                    log::info!("GODICO NATIVE: App Resumed - Smooth Flow");
+                    log::info!("GODICO NATIVE: App Resumed");
                 }
                 MainEvent::Destroy => {
                     log::info!("GODICO NATIVE: Shutdown");
