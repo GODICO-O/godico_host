62fc1d5059041a8a2568d5ad5cfd012d5039a369
GODICO-O <rizkiramadhan2300@gmail.com>
2026-07-01 18:18:16 +0700

    Auto-deploy: Sinkronisasi kode dan trigger CI/CD

--- rust_core/src/lib.rs
+++ rust_core/src/lib.rs
@@ -1,26 +1,11 @@
-use android_activity::{AndroidApp, MainEvent, PollEvent};
+use std::ffi::c_void;
 
 #[no_mangle]
-fn android_main(app: AndroidApp) {
-    android_logger::init_once(android_logger::Config::default().with_tag("GODICO_NATIVE"));
-    log::info!("GODICO NATIVE: Engine Starting...");
-
-    app.poll_events(Some(std::time::Duration::from_millis(16)), |event| {
-        match event {
-            PollEvent::Main(event) => match event {
-                // Semua event di v0.5.2 harus menggunakan sintaks { .. }
-                MainEvent::InitWindow { .. } => {
-                    log::info!("GODICO NATIVE: Window Ready");
-                }
-                MainEvent::Resume { .. } => {
-                    log::info!("GODICO NATIVE: App Resumed");
-                }
-                MainEvent::Destroy => {
-                    log::info!("GODICO NATIVE: Shutdown");
-                }
-                _ => (),
-            },
-            _ => (),
-        }
-    });
+#[allow(non_snake_case)]
+pub extern "C" fn Java_com_godico_devhub_MainActivity_startIpcServer(
+    _env: *mut c_void, _jclass: *mut c_void,
+) {
+    // Log sederhana untuk memastikan Rust terpanggil
+    android_logger::init_once(android_logger::Config::default().with_tag("GODICO_JNI"));
+    log::info!("JNI: Rust Engine Terpanggil dengan Sukses!");
 }
